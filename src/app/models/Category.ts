89cdca60ba72f531ac89eb0062cdf32269b210f4
Augusto.9237 <augusto.souza8330@gmail.com>
2022-12-03 20:34:04 -0300

    models mongo ok

--- src/app/models/Category.ts
+++ src/app/models/Category.ts
@@ -1,3 +1,12 @@
-import { model } from 'mongoose';
+import { model, Schema } from 'mongoose';
 
-export const Category = model('Category');
+export const Category = model('Category', new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    required: true
+  },
+}));
