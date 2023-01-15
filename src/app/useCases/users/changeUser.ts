79789f64bb9215bbedc8e99ab79f23042b60c711
Augusto.9237 <augusto.souza8330@gmail.com>
2023-01-15 19:45:40 -0300

    update

--- src/app/useCases/users/changeUser.ts
+++ src/app/useCases/users/changeUser.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from 'express';
 import { User } from '../../models/User';
+import bcrypt from 'bcrypt';
 
 export async function changeUser(req: Request, res: Response) {
   try {
     const { userId } = req.params;
     const { name, password, office } = req.body;
 
-    await User.findByIdAndUpdate(userId, { name, password, office });
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    await User.findByIdAndUpdate(userId, { name, password: passwordHash, office });
 
     res.sendStatus(204);
   } catch (error) {
--- src/router.ts
+++ src/router.ts
@@ -25,6 +25,7 @@ import { listUsers } from './app/useCases/users/listUsers';
 import { deleteUser } from './app/useCases/users/deleteUser';
 import { changeUser } from './app/useCases/users/changeUser';
 import { authUser } from './app/useCases/users/authUser';
+import { checkToken } from './app/useCases/users/chekToken';
 
 export const router = Router();
 
@@ -39,25 +40,7 @@ const upload = multer({
   }),
 });
 
-function checkToken(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
 
-  if(!token) {
-    return  res.status(401).json({ msg: 'Acesso negado!'});
-  }
-
-  try {
-    const secret = process.env.SECRET!;
-
-    jwt.verify(token, secret);
-
-    next();
-
-  } catch (error) {
-    res.status(400).json({msg: 'Token invalido'});
-  }
-}
 
 router.get('/categories', listCategories);
 
